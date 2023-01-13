4f68c23583e1325f455797d94c1e151f14b4fd01
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-13 07:28:52 -0500

    fix few errors

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ searchForm.addEventListener("submit", (event) => {
 });
 
 async function fetchAPI() {
-  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=50`;
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
   const response = await fetch(baseURL);
   const data = await response.json();
   generateHTML(data.hits);
