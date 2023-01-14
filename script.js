73a6e941df0cd9b6478857372e27a131093772fa
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-14 05:02:26 -0500

    add hamburger to index

--- script.js
+++ script.js
@@ -37,16 +37,3 @@ function generateHTML(results) {
   });
   searchResultDiv.innerHTML = generatedHTML;
 }
-
-document.querySelector('.first-button').addEventListener('click', function () {
-
-  document.querySelector('.animated-icon1').classList.toggle('open');
-  });
-  document.querySelector('.second-button').addEventListener('click', function () {
-  
-  document.querySelector('.animated-icon2').classList.toggle('open');
-  });
-  document.querySelector('.third-button').addEventListener('click', function () {
-  
-  document.querySelector('.animated-icon3').classList.toggle('open');
-  });
