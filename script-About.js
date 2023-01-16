b1f2e65c74f0044883796bf23e65e613b8976495
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-16 13:26:40 -0500

    css for about-us

--- script-About.js
+++ script-About.js
@@ -1,11 +1,11 @@
-//const APP_ID = "4486cb7f";
-//const APP_key = "07ca8aa9c321a1980cc6b75d4326acb8";
+const APP_ID = "4486cb7f";
+const APP_key = "07ca8aa9c321a1980cc6b75d4326acb8";
 const imgElement = document.querySelector('#image__center');
 
 document.addEventListener('DOMContentLoaded', function() {
     const cuisineTypes = ["italian", "chinese", "mexican", "indian", "french", "mediterranean","asian","american"];
     const randomCuisine = cuisineTypes[Math.floor(Math.random() * cuisineTypes.length)];
-    const ingredients = ["beef", "chicken", "pork", "fish", "vegetables", "tofu", "pizza"];
+    const ingredients = ["beef", "chicken", "pork", "fish", "vegetables", "tofu", "pizza", "milk", "sugar"];
     const randomIngredient = ingredients[Math.floor(Math.random() * ingredients.length)];
     const baseURL = `https://api.edamam.com/search?q=${randomIngredient}&app_id=${APP_ID}&app_key=${APP_key}&cuisine_type=${randomCuisine}`;
 
