af1e4b3bcbd0dce119a4ce49410818b9d552c7bf
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-16 15:00:53 -0500

    fix small bugs

--- script-About.js
+++ script-About.js
@@ -5,7 +5,7 @@ const imgElement = document.querySelector('#image__center');
 document.addEventListener('DOMContentLoaded', function() {
     const cuisineTypes = ["italian", "chinese", "mexican", "indian", "french", "mediterranean","asian","american"];
     const randomCuisine = cuisineTypes[Math.floor(Math.random() * cuisineTypes.length)];
-    const ingredients = ["beef", "chicken", "pork", "fish", "vegetables", "tofu", "pizza", "milk", "sugar"];
+    const ingredients = ["beef", "chicken", "pork", "fish", "vegetables", "tofu", "pizza", "milk", "sugar","cake","pie","icecream"];
     const randomIngredient = ingredients[Math.floor(Math.random() * ingredients.length)];
     const baseURL = `https://api.edamam.com/search?q=${randomIngredient}&app_id=${APP_ID}&app_key=${APP_key}&cuisine_type=${randomCuisine}`;
 
