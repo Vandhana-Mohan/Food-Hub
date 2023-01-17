0b23ba06870dc5a331b249c1b7f2a9128aee91e7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-17 06:17:27 -0500

    fetch tasty api

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const searchForm = document.querySelector(".foodHub__search__form");
 const searchResultDiv = document.querySelector(".foodHub__search__result");
 const container = document.querySelector(".foodHub__container");
-const videoContainer = document.querySelector(".foodHub__videos")
+const recipeContainer = document.querySelector(".foodHub__videos")
+
 let searchQuery = "";
 const APP_ID = "4486cb7f";
 const APP_key = "07ca8aa9c321a1980cc6b75d4326acb8";
@@ -36,7 +37,11 @@ function generateHTML(results) {
   searchResultDiv.innerHTML = generatedHTML;
 }
 
-function fetchInitialVideos() {
+//<p class="foodHub__search__result__data">Calories: ${result.recipe.calories.toFixed(2)}</p> */}
+//<p class="foodHub__search__result__data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : "No Data Found"}</p>
+//<p class="foodHub__search__result__data">Health labels: ${result.recipe.healthLabels}</p>
+
+function fetchInitialRecipes() {
   const options = {
     method: 'GET',
     headers: {
@@ -44,36 +49,40 @@ function fetchInitialVideos() {
       'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
     }
   };
+  
+  // Generate a random ingredient
+  const randomIngredient = generateRandomIngredient();
 
-  fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken', options)
+  fetch(`https://tasty.p.rapidapi.com/recipes/list?q=${randomIngredient}`, options)
       .then(response => response.json())
       .then(data => {
-        console.log(data)
-          generateVideo(data);
+         console.log(data)
+         generateRecipes(data);
       })
       .catch(error => console.error(error));
 }
 
-function generateVideo(data) {
-  let generatedvideo = "";
-  data.results.map((video) => {
-    generatedvideo += 
-    `<div class="foodHub__videos__item">
-    <video width="320" height="240" controls>
-      <source src="${video.video_url}" type="video/mp4">
-      <source src="${video.original_video_url}" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-    <h1 class="foodHub__videos__title">${video.keywords}</h1>
-    <p class="foodHub__videos__description">${video.draft_status}</p>
-</div>`;
-});
-    videoContainer.innerHTML = generatedvideo;
+function generateRandomIngredient() {
+  const ingredients = ['chicken', 'potato', 'beef', 'pork', 'tomato', 'spinach', 'carrot', 'onion','dessert','cookie','pie', 'egg'];
+  const randomIndex = Math.floor(Math.random() * ingredients.length);
+  return ingredients[randomIndex];
 }
 
-window.onload = fetchInitialVideos();
-
-//<p class="foodHub__search__result__data">Calories: ${result.recipe.calories.toFixed(2)}</p> */}
-//<p class="foodHub__search__result__data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : "No Data Found"}</p>
-//<p class="foodHub__search__result__data">Health labels: ${result.recipe.healthLabels}</p>
+function generateRecipes(data) {
+  let generatedRecipes = "";
+  data.results.map((recipe) => {
+    generatedRecipes += 
+      `<div class="foodHub__search__result__item">
+          <img class="foodHub__search__result__image" src="${recipe.thumbnail_url}" alt="${recipe.thumbnail_alt_text}">
+          <div class="foodHub__search__result__content">
+            <h1 class="foodHub__search__result__title">${recipe.name}</h1>
+          </div>
+       </div>`;
+  });
+  recipeContainer.innerHTML = generatedRecipes;
+}
+// <p class="foodHub__recipes__description">${recipe.description}</p>
+// <p class="foodHub__recipes__serving">Servings: ${recipe.servings_noun_plural}</p>
+// <p class="foodHub__recipes__time">Total Time: ${recipe.total_time_minutes} minutes</p>
+window.onload = fetchInitialRecipes();
 
