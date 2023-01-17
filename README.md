c3395efdf71d7a4ea07824a8e4c3f786fc4ce4a4
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-17 05:15:25 -0500

    Readme.md Done

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# Food-Hub
+# Foodie-Hub
+     > Food is symbolic of Love when words are inadequate - Alan D. Wolfelt
+
+This hub is the destination to all your recipe search and cooking needs
+
+Comfort food is the best and when you cook with love its delicious. 
+Does this ever come across your mind, I loved the fried rice in that restaurant..., 
+I wish could have my mom soup now *Foodie Hub* solves all your recipe issues
+
+# Project Overview
+This website allows to search recipe with any ingredient or cuisine.
+
+# Design
+Utmost care has been taken to keep design easy and flexible.
+
+#### Visit my website, find your recipe from a huge collection, _cook, eat and enjoy!!!_
+
+
+---
+#### Home Page
+![Home](./assets/Home.png)
+
+---
+
+#### Do leave me a feedback so I can improve. 
+![Feedback Form](./assets/Feedback%20Form.png)
+
+---
+
+#### Read our other reviews 
+![Reviews](./assets/Reviews.png)
+
+---
+
+#### Contact Us 
+![Contact Us](./assets/Contact%20Us.png)
+
+---
+
+#### About Us 
+![About Us](./assets/About%20Us.png)
+
+---
+
+#### Footer - Follow Us
+![Footer](./assets/Footer.png)
+
+---
+
+# Project setup
+---
+## Getting started
+
+1. Fork and clone this repository.
+2. Navigate to the cloned repository's directory on your command line. Then, run the following command:
+
+     > npm install
+
+
+This will install the libraries needed to run the tests.
+
+1. Open up the repository in VSCode.
+2. Contact me in Github - link in website footer.
+
+# Version 2 Updates
+
+Version 2 will have additional features so you can learn more about nutrition and calorie before searching for recipe.
+
+# Future Project Updates
+
+Currently working on
+
+    *Weather App Project
+    *Personalized Art App where you can display your art work and video links
+    *Nutrition App Project
+    *Build a game
+
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const searchForm = document.querySelector(".foodHub__search__form");
 const searchResultDiv = document.querySelector(".foodHub__search__result");
 const container = document.querySelector(".foodHub__container");
+const videoContainer = document.querySelector(".foodHub__videos")
 let searchQuery = "";
 const APP_ID = "4486cb7f";
 const APP_key = "07ca8aa9c321a1980cc6b75d4326acb8";
@@ -30,10 +31,49 @@ function generateHTML(results) {
           <h1 class="foodHub__search__result__title">${result.recipe.label}</h1>
           <a class="foodHub__search__result__link" target="_blank" href="${result.recipe.url}">View Recipe</a>
         </div>
-        <p class="foodHub__search__result__data">Calories: ${result.recipe.calories.toFixed(2)}</p>
-        <p class="foodHub__search__result__data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : "No Data Found"}</p>
-        <p class="foodHub__search__result__data">Health labels: ${result.recipe.healthLabels}</p>
       </div>`;
   });
   searchResultDiv.innerHTML = generatedHTML;
 }
+
+function fetchInitialVideos() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f5ea32695fmsha6dcac8242f9814p1dc272jsn8c0cd080f905',
+      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    }
+  };
+
+  fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken', options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+          generateVideo(data);
+      })
+      .catch(error => console.error(error));
+}
+
+function generateVideo(data) {
+  let generatedvideo = "";
+  data.results.map((video) => {
+    generatedvideo += 
+    `<div class="foodHub__videos__item">
+    <video width="320" height="240" controls>
+      <source src="${video.video_url}" type="video/mp4">
+      <source src="${video.original_video_url}" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+    <h1 class="foodHub__videos__title">${video.keywords}</h1>
+    <p class="foodHub__videos__description">${video.draft_status}</p>
+</div>`;
+});
+    videoContainer.innerHTML = generatedvideo;
+}
+
+window.onload = fetchInitialVideos();
+
+//<p class="foodHub__search__result__data">Calories: ${result.recipe.calories.toFixed(2)}</p> */}
+//<p class="foodHub__search__result__data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : "No Data Found"}</p>
+//<p class="foodHub__search__result__data">Health labels: ${result.recipe.healthLabels}</p>
+
