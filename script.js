bda272411d71d40145ec0f8b081a904a6d631847
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-23 06:47:21 -0500

    add reset to search

--- script.js
+++ script.js
@@ -6,15 +6,31 @@ const container = document.querySelector(".foodHub__container");
 const recipeContainer = document.querySelector(".foodHub__videos")
 const pagination = document.querySelector('ul');
 const heading = document.querySelector(".foodHub__recommendation")
-let currentPage = 1;
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.previous');
+const searchInput = document.querySelector(".foodHub__search__input");
+const clearBtn = document.querySelector(".clear-btn");
 let searchQuery = "";
+let currentPage = 1;
+
 // const apiKey = process.env.RAPIDAPI_KEY
 // const host = process.env.RAPIDAPI_HOST
 
 const APP_ID = '4486cb7f' // process.env.APP_ID_edamam;
 const APP_KEY = '07ca8aa9c321a1980cc6b75d4326acb8' //process.env.APP_key_edamam;
+
+searchInput.addEventListener("input", () => {
+  if (searchInput.value) {
+    clearBtn.style.display = "block";
+  } else {
+    clearBtn.style.display = "none";
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  searchInput.defaultValue = searchInput.value;
+  searchInput.value = "";
+});
 
 nextBtn.addEventListener('click', () => {
   currentPage++;
@@ -25,23 +41,34 @@ prevBtn.addEventListener('click', () => {
   currentPage--;
   fetchAPI(currentPage);
 });
-// currentPage = 1;
+
+window.addEventListener("beforeunload", () => {
+  searchInput.value = "";
+});
+
 searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  searchQuery = event.target.querySelector("input").value;
-  currentPage = 1;
-  fetchAPI(currentPage);
-  // Hide the recommended heading and images
-  heading.classList.add("hidden");
-  recipeContainer.classList.add("hidden");
+  if (!searchInput.value) {
+    event.preventDefault();
+  } else {
+    event.preventDefault();
+    searchQuery = event.target.querySelector("input").value;
+    currentPage = 1;
+    fetchAPI(currentPage);
+    // Hide the recommended heading and images
+    heading.classList.add("hidden");
+    recipeContainer.classList.add("hidden");
+  }
 });
 
 async function fetchAPI(currentPage) {
-  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=${(currentPage-1)*10}&to=${currentPage*10}`;
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${(currentPage-1)*10}&to=${currentPage*12}`;
   const response = await fetch(baseURL);
   const data = await response.json();
+  console.log(data)
+  if (data.status === "success") {
+    searchInput.value = "";
+  }
   generateHTML(data.hits);
-  console.log(data);
 }
 
 function generateHTML(results) {
@@ -67,16 +94,16 @@ function generateHTML(results) {
 //<p class="foodHub__search__result__data">Health labels: ${result.recipe.healthLabels}</p>
 
 // function fetchInitialRecipes() {
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Key': 'f5ea32695fmsha6dcac8242f9814p1dc272jsn8c0cd080f905',
-//       'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-//     }
-//   };
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'f5ea32695fmsha6dcac8242f9814p1dc272jsn8c0cd080f905',
+  //     'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+  //   }
+  // };
 
-//   // Generate a random ingredient
-//   const randomIngredient = generateRandomIngredient();
+  // Generate a random ingredient
+  // const randomIngredient = generateRandomIngredient();
 
 //   fetch(`https://tasty.p.rapidapi.com/recipes/list?q=${randomIngredient}`, options)
 //       .then(response => response.json())
@@ -113,5 +140,3 @@ function generateHTML(results) {
 // <p class="foodHub__recipes__serving">Servings: ${recipe.servings_noun_plural}</p>
 // <p class="foodHub__recipes__time">Total Time: ${recipe.total_time_minutes} minutes</p>
 // window.onload = fetchInitialRecipes();
-
-
