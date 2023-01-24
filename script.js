c1ca7ae3ebb09d90311b90860532084875f2cd78
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-01-24 09:27:12 -0500

    tasty api expired

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.previous');
 const searchInput = document.querySelector(".foodHub__search__input");
 const clearBtn = document.querySelector(".clear-btn");
+const searchIcon = document.querySelector(".fa-search");
 let searchQuery = "";
 let currentPage = 1;
 
@@ -21,11 +22,6 @@ searchInput.addEventListener("input", () => {
     clearBtn.style.display = "none";
   }
 });
-
-// clearBtn.addEventListener("click", () => {
-//   searchInput.defaultValue = searchInput.value;
-//   searchInput.value = "";
-// });
 
 nextBtn.addEventListener('click', () => {
   currentPage++;
@@ -42,27 +38,29 @@ window.addEventListener("beforeunload", () => {
 });
 
 searchForm.addEventListener("submit", (event) => {
-  if (!searchInput.value) {
-    event.preventDefault();
-  } else {
-    event.preventDefault();
-    searchQuery = event.target.querySelector("input").value;
-    currentPage = 1;
-    fetchAPI(currentPage);
-    // Hide the recommended heading and images
-    heading.classList.add("hidden");
-    recipeContainer.classList.add("hidden");
-  }
+  event.preventDefault();
+  searchQuery = event.target.querySelector("input").value;
+  currentPage = 1;
+  fetchAPI(currentPage);
+  // Hide the recommended heading and images
+  heading.classList.add("hidden");
+  recipeContainer.classList.add("hidden");
+});
+
+searchIcon.addEventListener("click", (event) => {
+  event.preventDefault();
+  searchQuery = searchInput.value;
+  currentPage = 1;
+  fetchAPI(currentPage);
+  // Hide the recommended heading and images
+  heading.classList.add("hidden");
+  recipeContainer.classList.add("hidden");
 });
 
 async function fetchAPI(currentPage) {
   const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${(currentPage-1)*12}&to=${currentPage*12}`;
   const response = await fetch(baseURL);
   const data = await response.json();
-  console.log(data)
-  if (data.status === "success") {
-    searchInput.value = "";
-  }
   generateHTML(data.hits);
 }
 
@@ -88,50 +86,41 @@ function generateHTML(results) {
 //<p class="foodHub__search__result__data">Diet label: ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels : "No Data Found"}</p>
 //<p class="foodHub__search__result__data">Health labels: ${result.recipe.healthLabels}</p>
 
-function fetchInitialRecipes() {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'f5ea32695fmsha6dcac8242f9814p1dc272jsn8c0cd080f905',
-      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-    }
-  };
+clearBtn.addEventListener("click", () => {
+  searchInput.value = "";
+  clearBtn.style.display = "none";
+});
 
+async function fetchInitialRecipes() {
   // Generate a random ingredient
-const randomIngredient = generateRandomIngredient();
-
-fetch(`https://tasty.p.rapidapi.com/recipes/list?q=${randomIngredient}`, options)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        generateRecipes(data);
-    })
-    .catch(error => console.error(error));
-    heading.classList.remove("hidden");
-    recipeContainer.classList.remove("hidden");
+  const randomIngredient = generateRandomIngredient();
+  const baseURL = `https://api.edamam.com/search?q=${randomIngredient}&app_id=${APP_ID}&app_key=${APP_KEY}&to=15`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateRecipes(data.hits);
+  heading.classList.remove("hidden");
+  recipeContainer.classList.remove("hidden");
 }
 
 function generateRandomIngredient() {
-  const ingredients = ['chicken', 'potato', 'beef', 'pork', 'tomato', 'spinach', 'carrot', 'onion','dessert','cookie','pie', 'egg','cheese','donut'];
+  const ingredients = ['chicken', 'potato', 'beef', 'pork', 'tomato', 'spinach', 'carrot', 'onion','dessert','cookies','pie', 'egg','cheese','donut'];
   const randomIndex = Math.floor(Math.random() * ingredients.length);
   return ingredients[randomIndex];
 }
 
 function generateRecipes(data) {
   let generatedRecipes = "";
-  
-  data.results.map((recipe) => {
+  data.map((recipe) => {
     generatedRecipes += 
       `<div class="foodHub__search__result__item">
-          <img class="foodHub__search__result__image" src="${recipe.thumbnail_url}" alt="${recipe.thumbnail_alt_text}">
+          <a href="${recipe.recipe.url}" target="_blank">
+            <img class="foodHub__search__result__image" src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+          </a>
           <div class="foodHub__search__result__content">
-            <h4 class="foodHub__search__result__title">${recipe.name}</h4>
+            <h4 class="foodHub__search__result__title">${recipe.recipe.label}</h4>
           </div>
        </div>`;
   });
   recipeContainer.innerHTML = generatedRecipes;
 }
-// <p class="foodHub__recipes__description">${recipe.description}</p>
-// <p class="foodHub__recipes__serving">Servings: ${recipe.servings_noun_plural}</p>
-// <p class="foodHub__recipes__time">Total Time: ${recipe.total_time_minutes} minutes</p>
 window.onload = fetchInitialRecipes();
